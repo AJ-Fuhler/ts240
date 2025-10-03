06d37d9329e6c0201ede944c2dd7865220495084
Aart Fuhler <aart.fuhler@gmail.com>
2025-10-03 17:14:34 -0500

    added lesson3 files

---
+++ lesson3/test.ts
@@ -0,0 +1,11 @@
+const numsAndStrings: Array<string | number> = [1, "2"];
+
+const matrix: Array<Array<number>> = [
+  [1, 2],
+  [3, 4],
+];
+
+const values = [1, "2", false]; //
+
+type FruitNames = "apple" | "banana" | "cherry";
+const fruits: Array<FruitNames> = ["apple", "banana", "mango"];
