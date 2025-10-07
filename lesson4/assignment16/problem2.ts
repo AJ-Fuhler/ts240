78858cac4a17a0c77bcdde5d08faad9f84dba3b5
Aart Fuhler <aart.fuhler@gmail.com>
2025-10-07 15:51:14 -0500

    added lesson 4 assignment 16 problem 2

---
+++ lesson4/assignment16/problem2.ts
@@ -0,0 +1,13 @@
+function safeGet<T>(array: T[], index: number): T | undefined {
+  if (index >= 0 && index < array.length) {
+    return array[index];
+  }
+
+  return undefined;
+}
+
+const names: string[] = ["John", "Jane"];
+const thirdName = safeGet(names, 2); // Should return undefined
+
+const numbers: number[] = [1, 2, 3];
+const number = safeGet(numbers, 1); // Should return 2
