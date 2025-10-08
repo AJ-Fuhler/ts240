f86c20f3e1b4cc54a1775d2f1c3a4aab208b07a0
Aart Fuhler <aart.fuhler@gmail.com>
2025-10-08 12:29:12 -0500

    added lesson4 assignment18

--- lesson4/test.ts
+++ lesson4/test.ts
@@ -1,38 +1,16 @@
-type Circle = {
-  kind: "circle";
-  radius: number;
+type MovieApiResponse = {
+  status: string;
+  data: {
+    title: string;
+    releaseYear: number;
+  };
 };
 
-type Square = {
-  kind: "square";
-  sideLength: number;
-};
-
-type Triangle = {
-  kind: "triangle";
-  base: number;
-  height: number;
-};
-
-type Shape = Circle | Square | Triangle;
-
-function describeShape(shape: Shape) {
-  let area: number;
-
-  switch (shape.kind) {
-    case "circle":
-      area = Math.PI * shape.radius ** 2;
-      break;
-    case "square":
-      area = shape.sideLength ** 2;
-      break;
-    case "triangle":
-      area = 0.5 * shape.base * shape.height;
-      break;
-    default:
-    const _exhaustiveCheck: never = shape;
-    throw new Error(`Invalid shape: ${JSON.stringify(_exhaustiveCheck)}`);
+function handleMovieApiResponse(response: unknown) {
+  if (!response || typeof response !== "object") {
+    console.log("No data received!");
+    return;
   }
 
-  console.log("The area is " + area);
+  const movieApiReponse = response as MovieApiResponse;
 }
